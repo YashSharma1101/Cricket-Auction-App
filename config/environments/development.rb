24063d90d5d031e326cbbecb95f14c88a6774547
YashSharma1101 <yashsharma0787@gmail.com>
2024-07-14 22:41:03 +0530

    Added smtp creds

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-  user_name: 'yashh1003@gmail.com',
-    password: 'dsso jbhg azie kwzw',
+    user_name: 'info.thecricauction@gmail.com',
+    password: 'pxsj anog qhpa bqms',
+  # user_name: 'yashh1003@gmail.com',
+  #   password: 'dsso jbhg azie kwzw',
     authentication: :plain,
     enable_starttls_auto: true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -66,8 +66,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'yashh1003@gmail.com',
-    password: 'dsso jbhg azie kwzw',
+    user_name: 'info.thecricauction@gmail.com',
+    password: 'pxsj anog qhpa bqms',
+    # user_name: 'yashh1003@gmail.com',
+    # password: 'dsso jbhg azie kwzw',
     authentication: :plain,
     enable_starttls_auto: true
   }

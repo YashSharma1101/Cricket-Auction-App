be90c462c4c3d95b476920102976a96abade4219
YashSharma1101 <yashsharma0787@gmail.com>
2024-07-15 12:55:10 +0530

    added logo to headers

--- app/mailers/auction_mailer.rb
+++ app/mailers/auction_mailer.rb
@@ -1,5 +1,6 @@
 class AuctionMailer < ApplicationMailer
   def player_report(user)
+    attachments.inline['ilead.png'] = File.read(Rails.root.join('app/assets/images/ilead.png'))
     @user = user
     mail(to: @user.email, subject: 'Welcome to iLead Cricket League - Your Auction Report')
   end
